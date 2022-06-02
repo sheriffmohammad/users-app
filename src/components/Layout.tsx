f44267e83127e473633c89501c4c19c77d67c48f
sherifelsaidy <sherif.elsaidy@hassanallam.com>
2022-06-02 23:32:53 +0200

    comments

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -8,16 +8,26 @@ type Props = {
 const Layout = ({ pageTitle, children }: Props) => {
     return (
         <div>
+
+            {/* Dynamic Page Title */}
             <title>{pageTitle}</title>
+
+            {/* Navigation */}
             <nav>
                 <ul>
                 </ul>
             </nav>
+
+            {/* Main Content */}
             <main>
+
                 <h1>{pageTitle}</h1>
+
                 {children}
+
             </main>
         </div>
     )
 }
+
 export default Layout
