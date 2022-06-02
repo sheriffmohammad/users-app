6f40c5b052f636e81734d08d1d04d35baddb5a8c
sherifelsaidy <sherif.elsaidy@hassanallam.com>
2022-06-02 23:19:57 +0200

    add the layout to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
+import Layout from './components/Layout';
 
 function App() {
   return (
+    <Layout pageTitle='Home Page'>
+
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -20,6 +23,7 @@ function App() {
         </a>
       </header>
     </div>
+    </Layout>
   );
 }
 
