fe43f1c86b3293c3c8ef5164b3d400f86fe6388f
sherifelsaidy <sherif.elsaidy@hassanallam.com>
2022-06-03 11:00:44 +0200

    sidebar menu items

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,17 +3,46 @@ import './Sidebar.scss'
 
 export default function Sidebar() {
     const [open, setOpen] = useState(true);
+    const Menu = [
+        {
+            title: 'Home',
+            src: 'icon-home'
+        },
+        {
+            title: 'Register',
+            src: 'icon-register',
+            gap: true
+        },
+        {
+            title: 'Log-In',
+            src: 'icon-log-in'
+        }
+    ]
     return (
-        <div>
-            <div className={'side-bar ' + `${open ? 'open' : 'closed'}`}>
+        <div className={'side-bar ' + `${open ? 'open' : 'closed'}`}>
 
-                {/* Arrow */}
-                <img onClick={() => setOpen(!open)} src="/assets/images/icon-arrow.png" className={`arrow bg-quaternary ${!open && 'rotate-180'}`} />
-                <div className='side-bar-header'>
-                    <img className="icon" src="assets/images/icon-user.png" />
-                    <h2 className='text-quaternary'>Users App</h2>
-                </div>
+            {/* Arrow */}
+            <img onClick={() => setOpen(!open)} src="/assets/images/icon-arrow.png" className={`arrow bg-quaternary ${!open && 'rotate-180'}`} />
+
+            {/* Header */}
+            <div className='side-bar-header'>
+
+                {/* Logo */}
+                <img className={`logo ${!open && 'center-absolute'}`} src="assets/images/icon-user.png" />
+
+                {/* Title */}
+                <h2 className={`text-white header-title ${!open && 'scale-0'} `}>Users App</h2>
             </div>
+
+            {/* Navigation menu*/}
+            <ul>
+                {Menu.map((item, index) => (
+                    <li className={`menu-item text-tertiary ${item.gap ? 'menu-item-gap' : ''}`} key={index}>
+                        <img className={`menu-icon ${!open && 'center-h'}`} src={`/assets/images/${item.src}.png`} />
+                        <span className={`${!open && 'hidden'}`}>{item.title}</span>
+                    </li>
+                ))}
+            </ul>
         </div >
     )
 }
