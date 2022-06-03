7fbac8bdd75729539b365b95351de816f1b2b48c
sherifelsaidy <sherif.elsaidy@hassanallam.com>
2022-06-03 18:24:30 +0200

    center the registration form

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -3,23 +3,21 @@ import './Register.scss'
 
 export default function Register() {
     return (
-        <section>
-            <div className="register">
-                <div className="col-1">
-                    <h2>Register</h2>
+        <div className="register">
+            <div className="col-1">
+                <h2>Register</h2>
 
-                    <form id='form' className='flex flex-col'>
-                        <input type="text" placeholder='username' />
-                        <input type="text" placeholder='password' />
-                        <input type="text" placeholder='confirm password' />
-                        <button className='btn'>Register</button>
-                    </form>
+                <form id='form' className='flex flex-col'>
+                    <input type="text" placeholder='username' />
+                    <input type="text" placeholder='password' />
+                    <input type="text" placeholder='confirm password' />
+                    <button className='btn'>Register</button>
+                </form>
 
-                </div>
-                <div className="col-2">
-                    <img src='assets/images/bg-form-2.jpg' alt="" />
-                </div>
             </div>
-        </section>
+            <div className="col-2">
+                <img src='assets/images/bg-form-2.jpg' alt="" />
+            </div>
+        </div>
     )
 }
