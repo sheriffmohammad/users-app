89343fab7a3b2994ea2f80c80a04d9b6bfe30758
sherifelsaidy <sherif.elsaidy@hassanallam.com>
2022-06-03 02:06:26 +0200

    sidebar with header

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -8,8 +8,11 @@ export default function Sidebar() {
             <div className={'side-bar ' + `${open ? 'open' : 'closed'}`}>
 
                 {/* Arrow */}
-                <img onClick={() => setOpen(!open)} src="/assets/images/side-bar-arrow.png" className={`arrow ${!open && 'rotate-180'}`} />
-                Test
+                <img onClick={() => setOpen(!open)} src="/assets/images/icon-arrow.png" className={`arrow bg-quaternary ${!open && 'rotate-180'}`} />
+                <div className='side-bar-header'>
+                    <img className="icon" src="assets/images/icon-user.png" />
+                    <h2 className='text-quaternary'>Users App</h2>
+                </div>
             </div>
         </div >
     )
