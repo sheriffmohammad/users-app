42728f275a1f81c9719acded18ae496b375e7086
sherifelsaidy <sherif.elsaidy@hassanallam.com>
2022-06-04 21:46:26 +0200

    test case

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import Home from './pages/Home';
+import { TranslateProvider } from './translate';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { act } from 'react-dom/test-utils';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+test('sidebar toggles on click', () => {
+  const queryClient = new QueryClient();
+  render(<TranslateProvider>
+    <QueryClientProvider client={queryClient}>
+      <App></App>
+    </QueryClientProvider>
+  </TranslateProvider>);
+  const languageElement = screen.getByText(/Arabic/i);
+  act(() => {
+    languageElement.click();
+  })
+  const PageElement = screen.getByText(/صفحة/i);
+  expect(PageElement).toBeInTheDocument();
 });
