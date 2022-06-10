e10275248c29da50974fa54c7bd905f5309ada35
sherifelsaidy <sherif.elsaidy@hassanallam.com>
2022-06-10 12:34:02 +0200

    Comments

--- cypress/e2e/NotFound.cy.js
+++ cypress/e2e/NotFound.cy.js
@@ -1,10 +1,16 @@
 
 describe('Not Found', () => {
+
+    // Go to a non-existent page
+
     beforeEach(() => {
         cy.visit('http://localhost:3000/random-page')
     })
 
     it('should show page not found', () => {
+
+        // An element with id "not-found" should exist
+
         cy.get('#not-found').should('exist');
     })
 })
