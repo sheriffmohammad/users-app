a7f085f691886ec4e38284b81101b2111a243c92
sherifelsaidy <sherif.elsaidy@hassanallam.com>
2022-06-10 13:55:48 +0200

    Put localhost link

--- src/api/apiClient.tsx
+++ src/api/apiClient.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 export default axios.create({
-    baseURL: "https://enable-tech-users-app-server.herokuapp.com",
+    baseURL: "https://enable-tech-users-app-server.herokuapp.com", // For local run use "http://localhost:5000",
     headers: {
         "Content-type": "application/json"
     }
